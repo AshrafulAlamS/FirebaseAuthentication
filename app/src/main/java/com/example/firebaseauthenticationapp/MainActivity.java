da1be1e77f55530a63be4d0eea39e61ea61f7381
AshrafulAlamS <mdashrafulalam44@gmail.com>
2020-10-30 19:54:31 +0600

    retrive profile data from Firebase Firestore

--- app/src/main/java/com/example/firebaseauthenticationapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthenticationapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firebaseauthenticationapp;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,14 +24,22 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private Button logoutBtn, verifyBtn;
-    TextView verifyMsg;
-    private FirebaseAuth firebaseAuth =FirebaseAuth.getInstance();
+    private TextView verifyMsg, fullName, email, phone;
+    private ImageView profileImage;
+    private final FirebaseAuth firebaseAuth =FirebaseAuth.getInstance();
     private final FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+    private FirebaseFirestore firestore;
+    private String userId ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +49,19 @@ public class MainActivity extends AppCompatActivity {
         logoutBtn=findViewById(R.id.btnLogout);
         verifyBtn=findViewById(R.id.verifyBtnId);
         verifyMsg=findViewById(R.id.verifyMsg);
+        fullName=findViewById(R.id.profileNameId);
+        email=findViewById(R.id.profileEmailId);
+        phone=findViewById(R.id.profilePhoneId);
+        profileImage=findViewById(R.id.profileImageId);
 
         //firebaseAuth = FirebaseAuth.getInstance();
         //firebaseUser = firebaseAuth.getCurrentUser();
+        firestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseAuth.getCurrentUser().getUid();
+
+        //Retrive data from FirebaseFirestore
+        retriveProfileData();
 
         //check the email verification
         if (!firebaseUser.isEmailVerified()){
@@ -105,5 +125,20 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onFailure: Email not sent" + e.getMessage());
             }
         });
+    }
+
+    //Retrive data from FirebaseFirestore
+    private void retriveProfileData(){
+        DocumentReference documentReference = firestore.collection("users").document(userId);
+        if (userId !=null){
+            documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                    email.setText(value.getString("email"));
+                    fullName.setText(value.getString("fName"));
+                    phone.setText(value.getString("phone"));
+                }
+            });
+        }
     }
 }
